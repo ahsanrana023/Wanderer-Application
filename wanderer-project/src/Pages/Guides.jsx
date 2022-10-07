bfdb52b019fadfdd6727457e1ad63871a82ca9dd
Ahsan Rana <91425343+ahsanrana023@users.noreply.github.com>
2022-10-07 14:59:36 +0500

    Guides-module
    Guides module initial working completed

--- wanderer-project/src/Pages/Guides.jsx
+++ wanderer-project/src/Pages/Guides.jsx
@@ -1,7 +1,24 @@
 import React from "react";
-
-function Guides() {
-  return <div>Guides</div>;
+import GuideCard from "../components/GuideCard.js";
+import data from "../guidedata.js";
+import "./guides.css";
+export default function Guides() {
+  const guides = data.map((item) => {
+    return (
+      <GuideCard
+        key={item.id}
+        name={item.name}
+        location={item.location}
+        phone={item.phone}
+        email={item.email}
+        isContact={item.isContact}
+      />
+    );
+  });
+  return (
+    <div className="guide">
+      <p className="guidedata">Total Guides Found: {data.length}</p>
+      {guides}
+    </div>
+  );
 }
-
-export default Guides;
